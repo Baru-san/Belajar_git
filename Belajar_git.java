90876ed7b5248f89408664f9e96acd7338dde59a
Muhammad AKbarul Ihsan <barullisan@gmail.com>
2023-11-04 14:52:36 +0700

    feat: edit

--- Belajar_git.java
+++ Belajar_git.java
@@ -2,5 +2,6 @@ class Belajar_git{
     public static void main(String[] args) {
         System.out.println("Mari Belajar GIt");
         System.out.println("Belajar Git itu Mudah");
+        System.out.println("tapi Ribet");
     }
 }
